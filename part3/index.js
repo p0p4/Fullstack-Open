bd3ed680f5643c6a6be3b5991fdeb2938088ac39
Tino Behnen <95407806+p0p4@users.noreply.github.com>
2024-08-02 15:29:32 +0300

    add part3.15

--- part3/index.js
+++ part3/index.js
@@ -49,10 +49,9 @@ app.get('/api/persons/:id', (request, response) => {
 });
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id;
-  persons = persons.filter((person) => person.id !== id);
-
-  response.status(204).end();
+  Person.findByIdAndDelete(request.params.id).then((person) => {
+    response.status(204).end();
+  });
 });
 
 app.post('/api/persons', (request, response) => {
