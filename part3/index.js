1e6941726dc86dea7f5a48386ea06bfcb5efcebb
Tino Behnen <95407806+p0p4@users.noreply.github.com>
2024-08-02 17:30:20 +0300

    add part3.22

--- part3/index.js
+++ part3/index.js
@@ -1,36 +1,36 @@
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-require('dotenv').config();
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+require('dotenv').config()
 
-const Person = require('./models/person');
+const Person = require('./models/person')
 
-const app = express();
+const app = express()
 
 // json parser
-app.use(express.json());
+app.use(express.json())
 // enables cross-origin resource sharing
-app.use(cors());
+app.use(cors())
 // grant public access to given directory
-app.use(express.static('dist'));
+app.use(express.static('dist'))
 
 // custom token for logging
-morgan.token('body', (request) => JSON.stringify(request.body));
+morgan.token('body', (request) => JSON.stringify(request.body))
 // logger based on tiny config
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
-);
+)
 
 app.get('/api/persons', (request, response, next) => {
   Person.find({})
     .then((persons) => {
-      response.json(persons);
+      response.json(persons)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 app.get('/info', (request, response, next) => {
-  const date = new Date();
+  const date = new Date()
   Person.find({})
     .then((persons) => {
       response.send(`
@@ -38,30 +38,30 @@ app.get('/info', (request, response, next) => {
       Phonebook has info for ${persons.length} people
       <br/>
       ${date}
-    </p>`);
+    </p>`)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then((person) => {
       if (person) {
-        response.json(person);
+        response.json(person)
       } else {
-        response.status(404).end();
+        response.status(404).end()
       }
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 app.post('/api/persons', (request, response, next) => {
-  const body = request.body;
+  const body = request.body
 
   if (!body.name || !body.number) {
     return response.status(400).json({
       error: 'name or number is missing',
-    });
+    })
   }
 
   Person.exists({ name: body.name })
@@ -69,26 +69,26 @@ app.post('/api/persons', (request, response, next) => {
       if (exists) {
         return response.status(400).json({
           error: 'name must be unique',
-        });
+        })
       }
 
       const person = new Person({
         name: body.name,
         number: body.number,
-      });
+      })
 
       person
         .save()
         .then((savedPerson) => {
-          response.json(savedPerson);
+          response.json(savedPerson)
         })
-        .catch((error) => next(error));
+        .catch((error) => next(error))
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const person = request.body;
+  const person = request.body
 
   Person.findByIdAndUpdate(request.params.id, person, {
     // run schema validations, mongoose: https://mongoosejs.com/docs/api/model.html#Model.findByIdAndUpdate()
@@ -97,45 +97,45 @@ app.put('/api/persons/:id', (request, response, next) => {
   })
     .then((updatedPerson) => {
       if (updatedPerson) {
-        response.json(updatedPerson);
+        response.json(updatedPerson)
       } else {
-        response.status(404).end();
+        response.status(404).end()
       }
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
-    .then((person) => {
-      response.status(204).end();
+    .then(() => {
+      response.status(204).end()
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' });
-};
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
-// next is required as a parameter even if not used!
+// next is required as a parameter for errorHandler even if not used!
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
+  console.error(error.message)
 
   if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' });
+    return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message });
+    return response.status(400).json({ error: error.message })
   }
 
-  return response.status(500).send({ error: 'internal server error' });
-};
+  return response.status(500).send({ error: 'internal server error' })
+}
 
 // if the request doesn't match any of the routes
-app.use(unknownEndpoint);
+app.use(unknownEndpoint)
 // handles any errors from the routes
-app.use(errorHandler);
+app.use(errorHandler)
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
