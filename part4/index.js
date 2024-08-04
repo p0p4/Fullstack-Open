eb62c9308eccb956cb21eca84f45ed1396cd3e44
Tino Behnen <95407806+p0p4@users.noreply.github.com>
2024-08-04 10:47:22 +0300

    add part4.2.3
    app

--- part4/index.js
+++ part4/index.js
@@ -1,17 +1,5 @@
+const app = require('./app')
 const config = require('./utils/config')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const blogsRouter = require('./controllers/blogs')
-const mongoose = require('mongoose')
-
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl)
-
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
