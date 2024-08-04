7b6eb1d0cb509d93144b66ccaeceb865f9f1f495
Tino Behnen <95407806+p0p4@users.noreply.github.com>
2024-08-04 13:00:52 +0300

    add part4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  // console.log(blogs)
+  const blogLikes = blogs.reduce((sum, blog) => sum + blog.likes, 0)
+  // console.log(blogLikes)
+
+  return blogLikes
+}
+
 module.exports = {
   dummy,
+  totalLikes,
 }
