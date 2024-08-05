9c47116f89b8335bac3776b82f7e8e6ecd926f18
Tino Behnen <95407806+p0p4@users.noreply.github.com>
2024-08-05 14:51:03 +0300

    add part4.8

---
+++ part4/tests/blog_api.test.js
@@ -0,0 +1,38 @@
+const { describe, test, after, beforeEach } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const helper = require('./test_helper')
+const supertest = require('supertest')
+const app = require('../app')
+
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+describe('verifying integrity of get requests', () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+
+    for (let blog of helper.initialBlogs) {
+      let blogObject = new Blog(blog)
+      await blogObject.save()
+    }
+  })
+
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    assert.strictEqual(response.body.length, helper.initialBlogs.length)
+  })
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
