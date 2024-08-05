b10bf1e5dc29c7a54059c9007161080664dcf7d2
Tino Behnen <95407806+p0p4@users.noreply.github.com>
2024-08-05 16:20:53 +0300

    add part4.10

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -9,36 +9,67 @@ const Blog = require('../models/blog')
 
 const api = supertest(app)
 
-describe('verifying integrity of get requests', () => {
-  beforeEach(async () => {
-    await Blog.deleteMany({})
+describe('blog api tests', () => {
+  describe('verifying integrity of get requests', () => {
+    beforeEach(async () => {
+      await Blog.deleteMany({})
 
-    for (let blog of helper.initialBlogs) {
-      let blogObject = new Blog(blog)
-      await blogObject.save()
-    }
+      for (let blog of helper.initialBlogs) {
+        let blogObject = new Blog(blog)
+        await blogObject.save()
+      }
+    })
+
+    test('blogs are returned as json', async () => {
+      await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+    })
+
+    test('all blogs are returned', async () => {
+      const response = await api.get('/api/blogs')
+
+      assert.strictEqual(response.body.length, helper.initialBlogs.length)
+    })
+
+    test('unique identifiers are properly named', async () => {
+      const response = await api.get('/api/blogs')
+
+      for (let blog of response.body) {
+        // ".prototype" can be used to ignore the properties of the object itself
+        assert(Object.prototype.hasOwnProperty.call(blog, 'id'))
+      }
+    })
   })
+  describe('verifying integrity of post requests', () => {
+    test('a valid blog can be added', async () => {
+      const testBlog = {
+        title: 'Test Blog',
+        author: 'Test Author',
+        url: 'http://testurl.com',
+        likes: 0,
+      }
 
-  test('blogs are returned as json', async () => {
-    await api
-      .get('/api/blogs')
-      .expect(200)
-      .expect('Content-Type', /application\/json/)
-  })
+      await api
+        .post('/api/blogs')
+        .send(testBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
 
-  test('all blogs are returned', async () => {
-    const response = await api.get('/api/blogs')
+      const blogsAtEnd = await helper.blogsInDb()
+      assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
 
-    assert.strictEqual(response.body.length, helper.initialBlogs.length)
-  })
+      const addedBlog = blogsAtEnd.find(
+        (blog) =>
+          blog.title === testBlog.title &&
+          blog.author === testBlog.author &&
+          blog.url === testBlog.url &&
+          blog.likes === testBlog.likes
+      )
 
-  test('unique identifiers are properly named', async () => {
-    const response = await api.get('/api/blogs')
-
-    for (let blog of response.body) {
-      // ".prototype" can be used to ignore the properties of the object itself
-      assert.strictEqual(Object.prototype.hasOwnProperty.call(blog, 'id'), true)
-    }
+      assert(addedBlog)
+    })
   })
 })
 
