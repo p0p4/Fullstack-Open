8636b07e6b48ee9b622c80f7fb0a02cc9b156ce4
Tino Behnen <95407806+p0p4@users.noreply.github.com>
2024-08-07 10:02:22 +0300

    add part4.15

--- part4/app.js
+++ part4/app.js
@@ -4,6 +4,7 @@ require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const mongoose = require('mongoose')
 
 const mongoUrl = config.MONGODB_URI
@@ -13,5 +14,6 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
