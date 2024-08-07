6a137188bfd1d722a42d23e66903240b06214c90
Tino Behnen <95407806+p0p4@users.noreply.github.com>
2024-08-07 14:41:53 +0300

    add part4.19

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,4 +1,5 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
@@ -7,14 +8,27 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
 blogsRouter.post('/', async (request, response) => {
   const { title, author, url, likes = 0 } = request.body
 
-  if (!title || !url) {
+  console.log(request.body)
+
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  } else if (!title || !url) {
     return response.status(400).json({ error: 'title and url are required' })
   }
 
-  const user = await User.findOne({})
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title,
