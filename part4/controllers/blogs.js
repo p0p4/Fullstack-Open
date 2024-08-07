fa92e2df9f483c6a6b1b69da817357f7a6ec2597
Tino Behnen <95407806+p0p4@users.noreply.github.com>
2024-08-07 13:02:19 +0300

    add part4.17

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,8 +1,9 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
@@ -13,15 +14,20 @@ blogsRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: 'title and url are required' })
   }
 
+  const user = await User.findOne({})
+
   const blog = new Blog({
     title,
     author,
     url,
     likes,
+    user: user.id,
   })
 
-  const savedBog = await blog.save()
-  response.status(201).json(savedBog)
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog.id)
+  await user.save()
+  response.status(201).json(savedBlog)
 })
 
 blogsRouter.put('/:id', async (request, response) => {
