a50314dcaa821630465080e8fda1e45b53eadb00
Tino Behnen <95407806+p0p4@users.noreply.github.com>
2024-08-10 19:19:51 +0300

    add part5.11.1

--- part5/src/services/blogs.js
+++ part5/src/services/blogs.js
@@ -26,13 +26,13 @@ const update = async (id, newObject) => {
   return response.data
 }
 
-const remove = (id) => {
+const remove = async (id) => {
   const config = {
     headers: { Authorization: token },
   }
 
-  const request = axios.delete(`${baseUrl}/${id}`, config)
-  return request.then((response) => response.data)
+  const response = await axios.delete(`${baseUrl}/${id}`, config)
+  return response.data
 }
 
 export default { setToken, getAll, create, update, remove }
