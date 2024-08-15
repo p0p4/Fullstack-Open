77c18bcf99e967177090f4afd51a08e712df38f8
Tino Behnen <95407806+p0p4@users.noreply.github.com>
2024-08-15 22:21:35 +0300

    add part6.14
    use json server for fetching initial anecdotes

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,9 +1,18 @@
+import { useEffect } from 'react'
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
+import anecdoteServices from './services/anecdotes'
+import { setAnecdotes } from './reducers/anecdoteReducer'
+import { useDispatch } from 'react-redux'
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    anecdoteServices.getAll().then((anecdotes) => dispatch(setAnecdotes(anecdotes)))
+  }, [dispatch])
+
   return (
     <div>
       <h2>Anecdotes</h2>
