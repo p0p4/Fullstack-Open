dacae0d6cbe90bebb5f6cb9caece37fcae1cf0c8
Tino Behnen <95407806+p0p4@users.noreply.github.com>
2024-08-15 15:26:15 +0300

    add part6.11
    redux toolkit for anecdote reducer and action creators

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,3 +1,5 @@
+import { createSlice, current } from '@reduxjs/toolkit'
+
 const anecdotesAtStart = [
   'If it hurts, do it more often',
   'Adding manpower to a late software project makes it later!',
@@ -19,40 +21,30 @@ const asObject = (anecdote) => {
 
 const initialState = anecdotesAtStart.map(asObject)
 
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case 'NEW_ANECDOTE':
-      return [...state, action.payload]
-    case 'VOTE': {
-      const id = action.payload.id
+const anecdoteSlicer = createSlice({
+  name: 'anecdotes',
+  initialState,
+  reducers: {
+    createAnecdote(state, action) {
+      const newAnecdote = {
+        content: action.payload,
+        id: getId(),
+        votes: 0,
+      }
+      console.log(current(state))
+      return [...state, newAnecdote]
+    },
+    addVote(state, action) {
+      const id = action.payload
       const anecdoteToChange = state.find((anecdote) => anecdote.id === id)
       const changedAnecdote = {
         ...anecdoteToChange,
         votes: anecdoteToChange.votes + 1,
       }
       return state.map((anecdote) => (anecdote.id !== id ? anecdote : changedAnecdote))
-    }
-    default:
-      return state
-  }
-}
-
-export const createAnecdote = (content) => {
-  return {
-    type: 'NEW_ANECDOTE',
-    payload: {
-      content,
-      id: getId(),
-      votes: 0,
     },
-  }
-}
+  },
+})
 
-export const addVote = (id) => {
-  return {
-    type: 'VOTE',
-    payload: { id },
-  }
-}
-
-export default reducer
+export const { createAnecdote, addVote } = anecdoteSlicer.actions
+export default anecdoteSlicer.reducer
