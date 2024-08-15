23d4e380e373f6fecb0eca3632194c3a9605672f
Tino Behnen <95407806+p0p4@users.noreply.github.com>
2024-08-15 14:31:31 +0300

    add part6.10
    use redux toolkit store

--- part6/redux-anecdotes/src/reducers/filterReducer.js
+++ part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,17 +1,14 @@
-const reducer = (state = '', action) => {
-  switch (action.type) {
-    case 'SET_FILTER':
+import { createSlice } from '@reduxjs/toolkit'
+
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState: '',
+  reducers: {
+    changeFilter(state, action) {
       return action.payload
-    default:
-      return state
-  }
-}
+    },
+  },
+})
 
-export const changeFilter = (filter) => {
-  return {
-    type: 'SET_FILTER',
-    payload: filter,
-  }
-}
-
-export default reducer
+export const { changeFilter } = filterSlice.actions
+export default filterSlice.reducer
