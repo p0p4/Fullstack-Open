2342a5f7ebab3fbfc4f6b8d3d902dc5ab122232f
Tino Behnen <95407806+p0p4@users.noreply.github.com>
2024-12-08 15:07:44 +0200

    add part7.6
    fix useField reset usage

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -78,9 +78,9 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const content = useField('content')
-  const author = useField('author')
-  const info = useField('info')
+  const { reset: resetContent, ...content } = useField('content')
+  const { reset: resetAuthor, ...author } = useField('author')
+  const { reset: resetInfo, ...info } = useField('info')
 
   const navigate = useNavigate()
 
@@ -97,9 +97,9 @@ const CreateNew = (props) => {
 
   const handleReset = (e) => {
     e.preventDefault()
-    content.reset()
-    author.reset()
-    info.reset()
+    resetContent()
+    resetAuthor()
+    resetInfo()
   }
 
   return (
